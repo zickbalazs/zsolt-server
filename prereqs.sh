af79b7086c9e44d4cd7f10e7b62b9c585a482647
Balázs Zick <balazs.zick2@gmail.com>
2026-02-19 14:49:36 +0100

    DBeaver added

--- prereqs.sh
+++ prereqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Creating networks..."
-docker network create qbit-network && docker network create jellyfin-network && docker network create proxy-network
+docker network create qbit-network && docker network create jellyfin-network && docker network create proxy-network && docker network create db-network
 echo "Starting containers...:"
 cd ./proxy && docker compose up -d
 cd ../
@@ -8,5 +8,6 @@ cd ./jellyfin && docker compose up -d
 cd ../
 cd ./qbittorrent && docker compose up -d
 cd ../
+cd ./dbeaver && docker compose up -d
 echo "Done"
 exit 0
